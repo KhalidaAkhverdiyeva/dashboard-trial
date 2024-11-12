674d7af7531a65ad2c8696da5b47148ce19f881a
Khalida <xalide.haqverdiyeva@gmail.com>
2024-11-12 18:38:59 +0400

    chore: folder structure change

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -124,7 +124,7 @@ const Header = () => {
         <div className="flex items-center gap-[10px]">
           <div className="py-[10px] flex justify-center items-center px-[14px] rounded-[8px] border-solid border-[1px] border-[#E5E7EA]">
             <img
-              src="src/assets/icons/ri_reset-left-line (1).png"
+              src="/icons/ri_reset-left-line (1).png"
               alt=""
               className="w-[20px] h-[20px]"
             />
@@ -132,32 +132,32 @@ const Header = () => {
           <div className="flex gap-[5px] py-[12px] px-[14px] bg-[#22385F] rounded-[8px] border-solid border-[1px] border-[#E5E7EA]">
             <p className="text-[#FFFFFF] font-[500]">Layihə əlavə et</p>
             <img
-              src="src/assets/icons/si_add-fill (1).png"
+              src="/icons/si_add-fill (1).png"
               alt=""
               className="w-[24px] h-[24px]"
             />
           </div>
           <div className="flex items-center gap-[5px] py-[12px] px-[14px]  rounded-[8px] border-solid border-[1px] border-[#E5E7EA]">
             <img
-              src="src/assets/icons/vscode-icons_file-type-excel (1).png"
+              src="/icons/vscode-icons_file-type-excel (1).png"
               alt=""
               className="w-[20px] h-[20px]"
             />
             <p className="text-[#403F3F] font-[500]">Upload.xlsx</p>
           </div>
           <img
-            src="src/assets/icons/ion_notifications (1).png"
+            src="/icons/ion_notifications (1).png"
             alt=""
             className="w-[25px] h-[25px]"
           />
           <div className="flex flex-col justify-center items-center">
             <img
-              src="src/assets/icons/WhatsApp Image 2024-05-31 at 14.59 2 (2).png"
+              src="/icons/WhatsApp Image 2024-05-31 at 14.59 2 (2).png"
               alt=""
               className="w-[32px] h-[32px]"
             />
             <img
-              src="src/assets/icons/tabler_caret-down-filled (4).png"
+              src="/icons/tabler_caret-down-filled (4).png"
               alt=""
               className="w-[20px] h-[15px]"
             />
@@ -169,7 +169,7 @@ const Header = () => {
         <div className="flex gap-[10px]">
           <div className="flex items-center gap-[5px] py-[3px] px-[4px] border-solid border-[1px] border-[#E5E7EA] rounded-[8px]">
             <img
-              src="src/assets/icons/iconoir_eye.png"
+              src="/icons/iconoir_eye.png"
               alt=""
               className="w-[24px] h-[24px]"
             />
@@ -177,7 +177,7 @@ const Header = () => {
           </div>
           <div className="flex items-center gap-[5px] py-[5px] px-[10px] border-solid border-[1px] border-[#E5E7EA] rounded-[8px]">
             <img
-              src="src/assets/icons/fluent_apps-list-20-regular.png"
+              src="/icons/fluent_apps-list-20-regular.png"
               alt=""
               className="w-[20px] h-[20px]"
             />
@@ -185,7 +185,7 @@ const Header = () => {
           </div>
           <div className="flex items-center gap-[5px] py-[5px] bg-[#AF2082] px-[10px] border-solid border-[1px] border-[#E5E7EA] rounded-[8px]">
             <img
-              src="src/assets/icons/fluent_text-bullet-list-square-search-20-regular.png"
+              src="/icons/fluent_text-bullet-list-square-search-20-regular.png"
               alt=""
               className="w-[20px] h-[20px]"
             />
@@ -195,7 +195,7 @@ const Header = () => {
         <div className="flex gap-[20px]">
           <div className="flex items-center gap-[5px] py-[3px] px-[4px] ">
             <img
-              src="src/assets/icons/f7_status.png"
+              src="/icons/f7_status.png"
               alt=""
               className="w-[20px] h-[20px]"
             />
@@ -203,7 +203,7 @@ const Header = () => {
           </div>
           <div className="flex items-center gap-[5px] py-[3px] px-[4px]">
             <img
-              src="src/assets/icons/stash_data-date-duotone.png"
+              src="/icons/stash_data-date-duotone.png"
               alt=""
               className="w-[26px] h-[26px]"
             />
@@ -215,7 +215,7 @@ const Header = () => {
         <div className="flex gap-[10px]">
           <div className="flex items-center gap-[5px] py-[3px] px-[5px] border-solid border-[1px] border-[#E5E7EA] rounded-[8px]">
             <img
-              src="src/assets/icons/Ellipse 16.png"
+              src="/icons/Ellipse 16.png"
               alt=""
               className="w-[10px] h-[10px]"
             />
@@ -223,7 +223,7 @@ const Header = () => {
           </div>
           <div className="flex items-center gap-[5px] py-[5px] px-[10px] border-solid border-[1px] border-[#E5E7EA] rounded-[8px]">
             <img
-              src="src/assets/icons/fluent_apps-list-20-regular.png"
+              src="/icons/fluent_apps-list-20-regular.png"
               alt=""
               className="w-[20px] h-[20px]"
             />
@@ -233,7 +233,7 @@ const Header = () => {
         <div className="flex gap-[20px]">
           <div className="flex items-center gap-[5px] py-[12px] px-[14px]  rounded-[8px] border-solid border-[1px] border-[#E5E7EA]">
             <img
-              src="src/assets/icons/vscode-icons_file-type-excel (1).png"
+              src="/icons/vscode-icons_file-type-excel (1).png"
               alt=""
               className="w-[20px] h-[20px]"
             />
@@ -241,7 +241,7 @@ const Header = () => {
           </div>
           <div className="flex items-center gap-[5px] py-[12px] px-[14px]  rounded-[8px] border-solid border-[1px] border-[#E5E7EA]">
             <img
-              src="src/assets/icons/vscode-icons_file-type-pdf2.png"
+              src="/icons/vscode-icons_file-type-pdf2.png"
               alt=""
               className="w-[20px] h-[20px]"
             />
